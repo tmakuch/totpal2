0f67bf6682f68e1b36083455873a1b1304353bc3
Zbyszek Tenerowicz <naugtur@gmail.com>
2020-03-20 18:48:21 +0100

    pick page fix

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ input, a {
 </style>
 `
 
+const head = `
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TOTPAL ${req.game}</title>
+        ${styles}
+    </head>
+`
+
 app.listen(3000, err => {
     if (err) throw err
     console.log(`> Ready On Server http://localhost:3000`)
@@ -41,7 +50,7 @@ app.get('/', (req, res) => {
 });
 app.get('/:game/pick', (req, res) => {
     const result = data[req.game][Math.floor(Math.random() * data[req.game].length)]
-    res.send(`${styles}<h1>${result}</h1><a href=`/${req.game}?clear=y`>clear</a>&nbsp;<a href="/${req.game}">back</a>`)
+    res.send(`${head}<h1>${result}</h1><a href=`/${req.game}?clear=y`>clear</a>&nbsp;<a href="/${req.game}">back</a>`)
 })
 app.get('/:game', (req, res) => {
     if (req.query.entry) {
@@ -53,12 +62,7 @@ app.get('/:game', (req, res) => {
         return res.redirect(`/${req.game}`);
     }
     res.send(`
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>TOTPAL ${req.game}</title>
-        ${styles}
-    </head>
+    ${head}
     <h2>Two of these people are lying.</h2>
     <form method="GET">
     <input type="text" name="entry" autofocus>
